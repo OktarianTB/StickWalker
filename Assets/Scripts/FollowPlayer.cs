bf814dd79ee4677651ffcf4625780e5bed98aaff
Oktarian <oktotb@gmail.com>
2019-07-08 16:30:46 +0200

    Inital setup, level generation and camera setup

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Start()
+    {
+        if (!player)
+        {
+            Debug.LogWarning("Player transform is missing from Camera");
+        }
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject pillar;
+    public GameObject pillarParent;
+
+    float minXSize = 0.6f;
+    float maxXSize = 1.2f;
+
+    float minSpacing = 2f;
+    float maxSpacing = 7f;
+
+    float distanceToLoad = 20f;
+    float yPosition = -4f;
+
+    void Start()
+    {
+        LoadNewSection(0);
+    }
+
+
+    public void LoadNewSection(float xStart)
+    {
+        float distance = distanceToLoad;
+        float currentX = xStart;
+        while(distance > 0)
+        {
+            float initialX = currentX;
+            
+            Vector3 pillarPosition = new Vector3(currentX, yPosition, 0);
+            pillar = Instantiate(pillar, pillarPosition, Quaternion.identity);
+
+            float pillarXSize = GetRandomPillarSize();
+            pillar.transform.localScale = new Vector3(pillarXSize, 4, 1);
+            pillar.transform.parent = pillarParent.transform;
+            pillar.name = "Pillar";
+
+            currentX += GetRandomSpacing();
+
+            currentX += pillarXSize;
+            distance -= currentX - initialX;
+        }
+    }
+
+    float GetRandomPillarSize()
+    {
+        float randomXSize = Random.Range(minXSize, maxXSize);
+        return randomXSize;
+    }
+
+    float GetRandomSpacing() // returns the space between 2 pillars
+    {
+        float randomSpacing = Random.Range(minSpacing, maxSpacing);
+        return randomSpacing;
+    }
+
+
+}
